429651144afaa2bdf13e22be4f597e11d4cc98d4
Lukasz Kuśnierczyk <>
2013-04-24 01:48:10 +0200

    Oczyszczono kod ze zbędnych linii i rozpoczęto dodawanie komentarzy w klasie ZmianaStatusu.

--- src/java/model/applications/ZmianaStanu.java
+++ src/java/model/applications/ZmianaStanu.java
@@ -1,11 +1,7 @@
 package model.applications;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.Employee;
@@ -14,44 +10,50 @@ import view.XMLGenerator;
 /**
  *
  * @author Lukasz
+ *
+ * Klasa sluzaca do zmiany statusu przesylki
  */
-
 public class ZmianaStanu implements model.ApplicationInterface {
-    
-    
-     private ResultSet resultSet;
-    
-    public void ZmianaStatusu(){
-        
-        this.resultSet = null;
-    }
- 
-@Override
-    public String getTitle(Employee employee) {
-        
-    return "Zmiana Statusu przesylki";
+
+    public void ZmianaStatusu() {
     }
 
+    /**
+     * Metoda wyswietlajaca nazwe w oknie aplikacji
+     *
+     * @param employee
+     * @return
+     */
+    @Override
+    public String getTitle(Employee employee) {
+
+        return "Zmiana Statusu przesylki";
+    }
+
+    /**
+     *
+     * @param employee
+     * @param httpServletResponse
+     * @param xmlGenerator
+     * @param parameterMap
+     * @param httpSession
+     */
     @Override
     public void printApplication(Employee employee, HttpServletResponse httpServletResponse, XMLGenerator xmlGenerator, Map<String, String[]> parameterMap, HttpSession httpSession) {
-       
-        xmlGenerator.printStartTag("form", "actoin","","method","POST");
-        
+
+        xmlGenerator.printStartTag("form", "actoin", "", "method", "POST");
+
         xmlGenerator.println("Numer Przesyłki");
-        xmlGenerator.printEmptyElement("input", "name","kod_nadawcy","type","text");
+        xmlGenerator.printEmptyElement("input", "name", "kod_nadawcy", "type", "text");
         xmlGenerator.printEmptyElement("br");
-        
+
         xmlGenerator.println("Obecny Status Przesyłki");
-        xmlGenerator.printEmptyElement("input", "name","kod_odbiorcy","type","text");
+        xmlGenerator.printEmptyElement("input", "name", "kod_odbiorcy", "type", "text");
         xmlGenerator.printEmptyElement("br");
-        
+
         xmlGenerator.printEmptyElement("input", "name", "submit", "type", "submit", "value", "Zatwierdź");
         xmlGenerator.printEmptyElement("br");
-        
+
         xmlGenerator.printEndTag();
     }
-    
-    
 }
-
-
