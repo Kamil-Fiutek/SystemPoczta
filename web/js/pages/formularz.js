5e5cc8fe39625b222468d781559b6616a4804add
Łukasz Fornalczyk <>
2013-04-28 20:53:19 +0200

    Zmieniono sposób przechodzenia między formularzami.

--- web/js/pages/formularz.js
+++ web/js/pages/formularz.js
@@ -1,26 +1,41 @@
-$(function() {
-    
-    //reloadTabs();
-    
-    $(".tabEvaluator").bind("click", function() {
+$(function() 
+{
+    $(".tabEvaluator").bind("click", function() 
+    {
         reloadTabs();
         
         $(this).parents(".tab").removeClass("tabActive");
         
         return false;
     });
-    
 });
 
-function reloadTabs() {
+function reloadTabs() 
+{
+    var fadeTime = 1500;
     $(".tab").removeClass("tabActive");
     
     var value = $("select[name='typ_przesylki']").val();
     
     if(value == "1")
-        $(".tab.tabList").addClass("tabActive");
+    {
+        //$(".tab.tabList").addClass("tabActive");
+        $(".tab.tabList").fadeIn(fadeTime);
+        $(".tab.tabPaczka").fadeOut(fadeTime);
+        $(".tab.tabPrzekaz").fadeOut(fadeTime);
+    }
     if(value == "2")
-        $(".tab.tabPaczka").addClass("tabActive");
+    {
+        //$(".tab.tabPaczka").addClass("tabActive");
+        $(".tab.tabPaczka").fadeIn(fadeTime);
+        $(".tab.tabList").fadeOut(fadeTime);
+        $(".tab.tabPrzekaz").fadeOut(fadeTime);
+    }
     if(value == "3")
-        $(".tab.tabPrzekaz").addClass("tabActive");
+    {
+        //$(".tab.tabPrzekaz").addClass("tabActive");
+        $(".tab.tabPrzekaz").fadeIn(fadeTime);
+        $(".tab.tabList").fadeOut(fadeTime);
+        $(".tab.tabPaczka").fadeOut(fadeTime);
+    }   
 }
