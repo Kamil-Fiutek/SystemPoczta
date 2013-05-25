53e7e478317265bb05aa0ddd716abce4937b2998
Kamil-Fiutek <komando15@gmail.com>
2013-05-26 01:55:45 +0300

    Create ModyfikowaniePlacowek.java

---
+++ src/java/model/applications/ModyfikowaniePlacowek.java
@@ -0,0 +1,38 @@
+
+package model.applications;
+
+import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Employee;
+import view.XMLGenerator;
+
+/**
+ *
+ * @author Kamil Fiutek
+ * @version  0.01
+ */
+public class ModyfikowaniePlacowek implements model.ApplicationInterface{
+    /**
+ * 
+ * @param employee
+ * @return 
+ */
+    @Override
+    public String getTitle(Employee employee) {
+        
+        return "Modyfikacja Placówki";
+    }
+    /**
+     * 
+     * @param employee
+     * @param httpServletResponse
+     * @param xmlGenerator
+     * @param parameterMap
+     * @param httpSession 
+     */
+    @Override
+    public void printApplication(Employee employee, HttpServletResponse httpServletResponse, XMLGenerator xmlGenerator, Map<String, String[]> parameterMap, HttpSession httpSession) {
+        xmlGenerator.println("Jeszcze cegła");
+    }
+}
